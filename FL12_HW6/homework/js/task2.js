febe79f480a467a5b70c069f944d32b379e7b8f3
Artem Bohush <47715236+Artem-Bohush@users.noreply.github.com>
2019-12-20 20:57:11 +0200

    Add files via upload

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,17 +1,11 @@
-let a = prompt('Enter number a:');
-let b = prompt('Enter number b:');
-let c = prompt('Enter number c:');
-
-if (a === '' || a === undefined ||
-  b === '' || b === undefined ||
-  c === '' || c === undefined) {
+const a = +prompt('Enter number a:');
+const b = +prompt('Enter number b:');
+const c = +prompt('Enter number c:');
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
   alert('input values should be ONLY numbers');
-} else if (a === 0 || b === 0 || c === 0) {
+} else if (a <= 0 || b <= 0 || c <= 0) {
   alert('A triangle must have 3 sides with a positive definite length');
 } else {
-  a = +a;
-  b = +b;
-  c = +c;
   if (a + b > c && a + c > b && b + c > a) {
     if (a === b && b === c) {
       alert('Equilateral triangle');
