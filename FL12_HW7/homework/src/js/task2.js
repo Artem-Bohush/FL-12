3110a719641ab2be91f7586f6fc853a93ab8a6f0
Artem Bohush <47715236+Artem-Bohush@users.noreply.github.com>
2019-12-27 18:07:10 +0200

    Update task2.js

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,5 +1,6 @@
 if (confirm('Do you want to play a game?')) {
-  let isContinue = true;
+  let isContinue = false;
+  let isAgain = true;
   const gameInfo = {
     attemptsLeft: 3,
     totalPrize: 0,
@@ -7,7 +8,8 @@ if (confirm('Do you want to play a game?')) {
     1: 50,
     2: 25
   }
-  do {
+  firstGame:
+  while (isAgain) {
     let randNum = Math.round(-0.5 + Math.random() * 9);
     console.log(randNum);
     for (let i = 0; i < 3; i++) {
@@ -19,37 +21,50 @@ if (confirm('Do you want to play a game?')) {
       );
       if (userNum !== randNum && i === 2) {
         alert(`Thank you for your participation. Your prize is: ${gameInfo.totalPrize}$`);
-        isContinue = confirm('Do you want to paly again?');
+        isAgain = confirm('Do you want to paly again?');
         break;
       } else if (userNum === randNum) {
-        switch (i) {
-          case 0:
-            gameInfo.totalPrize += gameInfo[i];
-            isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
-              'Do you want to continue?');
-            break;
-          case 1:
-            gameInfo.totalPrize += gameInfo[i];
-            isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
-              'Do you want to continue?');
-            break;
-          default:
-            gameInfo.totalPrize += gameInfo[i];
-            isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
-              'Do you want to continue?');
+        if (i === 0) {
+          gameInfo.totalPrize += gameInfo[i];
+          isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
+            'Do you want to continue?');
+          if (isContinue) {
+            break firstGame;
+          } else {
+            alert(`Thank you for your participation. Your prize is: ${gameInfo.totalPrize}$`);
+            isAgain = confirm('Do you want to paly again?');
+          }
+        } else if (i === 1) {
+          gameInfo.totalPrize += gameInfo[i];
+          isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
+            'Do you want to continue?');
+          if (isContinue) {
+            break firstGame;
+          } else {
+            alert(`Thank you for your participation. Your prize is: ${gameInfo.totalPrize}$`);
+            isAgain = confirm('Do you want to paly again?');
+          }
+        } else {
+          gameInfo.totalPrize += gameInfo[i];
+          isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
+            'Do you want to continue?');
+          if (isContinue) {
+            break firstGame;
+          } else {
+            alert(`Thank you for your participation. Your prize is: ${gameInfo.totalPrize}$`);
+            isAgain = confirm('Do you want to paly again?');
+          }
         }
         break;
       }
       gameInfo.attemptsLeft -= 1;
     }
     gameInfo.attemptsLeft = 3;
-  } while (isContinue)
-  alert(`Thank you for your participation. Your prize is: ${gameInfo.totalPrize}$`);
-  isContinue = confirm('Do you want to paly again?');
+  }
   while (isContinue) {
+    gameInfo.attemptsLeft = 3;
     let randNum = Math.round(-0.5 + Math.random() * 13);
     console.log(randNum);
-    // gameInfo.attemptsLeft = 3;
     for (let i = 0; i < 3; i++) {
       let userNum = +prompt(
         'Choose a roulette pocket number from 0 to 12\n' +
@@ -66,23 +81,22 @@ if (confirm('Do you want to play a game?')) {
           case 0:
             gameInfo.totalPrize += gameInfo[i] * 2;
             isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
-              'Do you want to continue?');
+              'Do you want to paly again?');
             break;
           case 1:
             gameInfo.totalPrize += gameInfo[i] * 2;
             isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
-              'Do you want to continue?');
+              'Do you want to paly again?');
             break;
           default:
             gameInfo.totalPrize += gameInfo[i] * 2;
             isContinue = confirm(`Congratulation, you won! Your prize is: ${gameInfo.totalPrize}$. ` +
-              'Do you want to continue?');
+              'Do you want to paly again?');
         }
         break;
       }
       gameInfo.attemptsLeft -= 1;
     }
-    gameInfo.attemptsLeft = 3;
   }
 } else {
   alert('You did not become a billionaire, but can');
