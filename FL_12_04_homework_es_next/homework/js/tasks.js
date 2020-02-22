0855abd5398f0b975883749b9b79664dc30666ea
Artem Bohush <47715236+Artem-Bohush@users.noreply.github.com>
2020-02-22 13:45:51 +0200

    Add files via upload

---
+++ FL_12_04_homework_es_next/homework/js/tasks.js
@@ -0,0 +1,22 @@
+// ----------------------------Task1----------------------------------
+
+// function maxElement(arr) {
+//   return Math.max(...arr);
+// }
+
+// const array = [1, 2, 3, 4, 56, 7, 8, 76, 0, -4, 241, 5, 567, 2];
+
+// console.log(maxElement(array));
+
+// ----------------------------Task2----------------------------------
+
+// function copyArray(arr) {
+//   return new Array(...arr);
+// }
+
+// const array = [1, 2, 3, 4, 5];
+// const copiedArray = copyArray(array);
+// console.log(copiedArray, array);
+// console.log(copiedArray === array);
+
+// ----------------------------Task3----------------------------------
