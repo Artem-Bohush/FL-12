2b6ad25565af75de86e1708decaf9688fbc408d9
Artem Bohush <47715236+Artem-Bohush@users.noreply.github.com>
2020-02-22 15:17:10 +0200

    Add files via upload

--- FL_12_04_homework_es_next/homework/js/tasks.js
+++ FL_12_04_homework_es_next/homework/js/tasks.js
@@ -34,3 +34,34 @@
 // console.log(testObj === enhancedObj);
 // console.log(testObj);
 // console.log(enhancedObj);
+
+// ----------------------------Task4----------------------------------
+/*
+function regroupObject(obj) {
+  const regroupedObj = {};
+  const {name, id, age, university } = obj;
+  console.log(name, age);
+}
+
+const oldObj = {
+  name: 'Someone',
+  details: {
+    id: 1,
+    age: 21,
+    university: 'UNI'
+  }
+}
+
+regroupObject(oldObj);
+*/
+
+// ----------------------------Task5----------------------------------
+
+function findUniqueElements(arr) {
+  const uniqueElements = new Set();
+  arr.forEach(el => uniqueElements.add(el));
+  return Array.from(uniqueElements);
+}
+
+const array = [1, 1, 23, 3, 4, 5, 6, 5, 4, 23, 2, 1, 1, 1, 1, 1];
+console.log(findUniqueElements(array));
